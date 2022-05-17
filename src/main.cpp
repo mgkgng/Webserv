88c82edb122615a4f91bbf957e7bf926e0eafbe1
Jolan <rathelotj@gmail.com>
2022-05-17 11:50:15 +0200

    numbers are now correctly parsed

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,8 @@
 int main() {
 	try {
 		JSON test = JSON("test.json");
-		std::cout << test.getBooleans().at("test") << std::endl;
-		std::cout << test.getBooleans().at("test2") << std::endl;
-		
+		std::cout << test.getNumbers().at("nice") << std::endl;
+		std::cout << test.getObjects().at("test").getNumbers().at("test2") << std::endl;
 	} catch (std::exception & e) {
 		std::cout << e.what() << std::endl;
 	}
